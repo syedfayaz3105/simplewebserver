946c2c42e61b9768bbfdeda85cde992a67fda06b
syedfayaz3105 <farhanahussain3105@gmail.com>
2024-03-10 19:39:41 +0530

    Success

---
+++ web.py
@@ -0,0 +1,21 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+content = """
+<html>
+<head>
+<title>MY webserver</title>
+</head>
+<h1>welcome</h1>
+</body>
+</html>
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',80)
+httpd = HTTPServer(server_address,myhandler)
+print("my webserver is running...")
+httpd.serve_forever()
